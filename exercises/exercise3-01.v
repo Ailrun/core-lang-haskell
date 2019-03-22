704e3ab30cd2fcca56c1419b85ad70bd7f86eef8
Junyoung Clare Jang <jjc9310@gmail.com>
2019-03-22 15:34:46 -0400

    Update coq files

--- exercises/exercise3-01.v
+++ exercises/exercise3-01.v
@@ -2,78 +2,78 @@ Require Import List.
 
 Import ListNotations.
 
-Inductive aExpr :=
-| Num : nat -> aExpr
-| Plus : aExpr -> aExpr -> aExpr
-| Mult : aExpr -> aExpr -> aExpr
+Inductive a_expr :=
+| Num : nat -> a_expr
+| Plus : a_expr -> a_expr -> a_expr
+| Mult : a_expr -> a_expr -> a_expr
 .
 
-Fixpoint aInterpret (e : aExpr) : nat :=
+Fixpoint a_interpret (e : a_expr) : nat :=
   match e with
   | Num n => n
-  | Plus e1 e2 => aInterpret e1 + aInterpret e2
-  | Mult e1 e2 => aInterpret e1 * aInterpret e2
+  | Plus e1 e2 => a_interpret e1 + a_interpret e2
+  | Mult e1 e2 => a_interpret e1 * a_interpret e2
   end
 .
 
-Inductive aInstruction :=
-| INum : nat -> aInstruction
-| IPlus : aInstruction
-| IMult : aInstruction
+Inductive a_instruction :=
+| INum : nat -> a_instruction
+| IPlus : a_instruction
+| IMult : a_instruction
 .
 
-Fixpoint aEval' (aIs : list aInstruction) (stk : list nat) : option nat :=
-  match aIs with
+Fixpoint a_eval' (a_is : list a_instruction) (stk : list nat) : option nat :=
+  match a_is with
   | [] =>
     match stk with
     | [n] => Some n
     | _ => None
     end
-  | INum n :: aIs' => aEval' aIs' (n :: stk)
-  | IPlus :: aIs' =>
+  | INum n :: a_is' => a_eval' a_is' (n :: stk)
+  | IPlus :: a_is' =>
     match stk with
-    | n0 :: n1 :: stk' => aEval' aIs' (n1 + n0 :: stk')
+    | n0 :: n1 :: stk' => a_eval' a_is' (n1 + n0 :: stk')
     | _ => None
     end
-  | IMult :: aIs' =>
+  | IMult :: a_is' =>
     match stk with
-    | n0 :: n1 :: stk' => aEval' aIs' (n1 * n0 :: stk')
+    | n0 :: n1 :: stk' => a_eval' a_is' (n1 * n0 :: stk')
     | _ => None
     end
   end.
 
-Definition aEval (state : list aInstruction * list nat) : option nat :=
+Definition a_eval (state : list a_instruction * list nat) : option nat :=
   match state with
-  | (aIs, stk) => aEval' aIs stk
+  | (a_is, stk) => a_eval' a_is stk
   end
 .
 
-Fixpoint aCompile (e : aExpr) : list aInstruction :=
+Fixpoint a_compile (e : a_expr) : list a_instruction :=
   match e with
   | Num n => [INum n]
-  | Plus e1 e2 => aCompile e1 ++ aCompile e2 ++ [IPlus]
-  | Mult e1 e2 => aCompile e1 ++ aCompile e2 ++ [IMult]
+  | Plus e1 e2 => a_compile e1 ++ a_compile e2 ++ [IPlus]
+  | Mult e1 e2 => a_compile e1 ++ a_compile e2 ++ [IMult]
   end
 .
 
-Lemma interpret_compile_equiv_general : forall (aIs : list aInstruction) (stk : list nat) (e : aExpr),
-    aEval (aIs, aInterpret e :: stk) = aEval (aCompile e ++ aIs, stk)
+Lemma interpret_compile_equiv_general : forall (a_is : list a_instruction) (stk : list nat) (e : a_expr),
+    a_eval (a_is, a_interpret e :: stk) = a_eval (a_compile e ++ a_is, stk)
 .
 Proof.
-  intros; generalize dependent stk; generalize dependent aIs.
+  intros; generalize dependent stk; generalize dependent a_is.
   induction e; intros; simpl; auto; try (
     repeat rewrite app_assoc_reverse;
-    unfold aEval in IHe1; unfold aEval in IHe2;
+    unfold a_eval in IHe1; unfold a_eval in IHe2;
     rewrite <- IHe1; rewrite <- IHe2; now auto
   ).
 Qed.
 
-Theorem interpret_compile_equiv : forall (e : aExpr),
-    Some (aInterpret e) = aEval (aCompile e, [])
+Theorem interpret_compile_equiv : forall (e : a_expr),
+    Some (a_interpret e) = a_eval (a_compile e, [])
 .
 Proof.
   intros.
-  replace (aCompile e) with (aCompile e ++ []);
+  replace (a_compile e) with (a_compile e ++ []);
     auto with datatypes.
   rewrite <- interpret_compile_equiv_general.
   now auto.
--- exercises/exercise3-02.v
+++ exercises/exercise3-02.v
@@ -1,148 +1,178 @@
 Require Import Basics.
 Require Import DecBool.
-Require Import List.
+Require Import Nat.
 Require Import String.
+Require Import List.
 
 Import ListNotations.
 
-Inductive aExpr :=
-| Num : nat -> aExpr
-| Plus : aExpr -> aExpr -> aExpr
-| Mult : aExpr -> aExpr -> aExpr
-| AVar : string -> aExpr
-| ALet : list (string * aExpr) -> aExpr -> aExpr
+Inductive a_expr :=
+| Num : nat -> a_expr
+| Plus : a_expr -> a_expr -> a_expr
+| Mult : a_expr -> a_expr -> a_expr
+| AVar : string -> a_expr
+| ALet : list (string * a_expr) -> a_expr -> a_expr
 .
 
-Definition beq_string s1 s2 : bool := if string_dec s1 s2 then true else false.
+Definition a_int_env := list (string * nat).
 
-Fixpoint aInterpret' (e : aExpr) (env : list (string * option nat)) : option nat :=
+Fixpoint a_interpret' (e : a_expr) (env : a_int_env) : option nat :=
   match e with
   | Num n => Some n
   | Plus e1 e2 =>
-    match (aInterpret' e1 env, aInterpret' e2 env) with
+    match (a_interpret' e1 env, a_interpret' e2 env) with
       | (Some n1, Some n2) => Some (n1 + n2)
       | _ => None
     end
   | Mult e1 e2 =>
-    match (aInterpret' e1 env, aInterpret' e2 env) with
+    match (a_interpret' e1 env, a_interpret' e2 env) with
       | (Some n1, Some n2) => Some (n1 * n2)
       | _ => None
     end
-  | ALet bs bodyE => aInterpret' bodyE ((map (fun b => match b with (bId, bExpr) => (bId, aInterpret' bExpr env) end) bs) ++ env)
+  | ALet bs bodyE =>
+    let b_folder :=
+        fun acc b =>
+          match a_interpret' (snd b) env with
+          | Some n => ((compose (cons (fst b, n)) (fst acc)), snd acc)
+          | _ => (fst acc, false)
+          end
+    in
+    let (make_b_env, is_success) := fold_left b_folder bs (id, true) in
+    if is_success
+    then a_interpret' bodyE (make_b_env [] ++ env)
+    else None
   | AVar id =>
-    match find (compose (flip beq_string id) fst) env with
-    | Some (_, Some n) => Some n
+    match find (compose (eqb id) fst) env with
+    | Some (_, n) => Some n
     | _ => None
     end
   end
 .
 
-Definition aInterpret (e : aExpr) : option nat := aInterpret' e [].
+Definition a_interpret (e : a_expr) : option nat := a_interpret' e [].
 
-Inductive aInstruction :=
-| INum : nat -> aInstruction
-| IPlus : aInstruction
-| IMult : aInstruction
-| ICopy : nat -> aInstruction
-| ISlide : nat -> aInstruction
+Inductive a_instruction :=
+| INum : nat -> a_instruction
+| IPlus : a_instruction
+| IMult : a_instruction
+| ICopy : nat -> a_instruction
+| ISlide : nat -> a_instruction
+| IFail : a_instruction
 .
 
-Fixpoint aEval' (aIs : list aInstruction) (stk : list nat) : option nat :=
-  match aIs with
+Definition a_stack := list nat.
+
+Fixpoint a_eval' (a_is : list a_instruction) (stk : a_stack) : option nat :=
+  match a_is with
   | [] =>
     match stk with
     | [n] => Some n
     | _ => None
     end
-  | INum n :: aIs' => aEval' aIs' (n :: stk)
-  | IPlus :: aIs' =>
+  | INum n :: a_is' => a_eval' a_is' (n :: stk)
+  | IPlus :: a_is' =>
     match stk with
-    | n0 :: n1 :: stk' => aEval' aIs' (n1 + n0 :: stk')
+    | n0 :: n1 :: stk' => a_eval' a_is' (n1 + n0 :: stk')
     | _ => None
     end
-  | IMult :: aIs' =>
+  | IMult :: a_is' =>
     match stk with
-    | n0 :: n1 :: stk' => aEval' aIs' (n1 * n0 :: stk')
+    | n0 :: n1 :: stk' => a_eval' a_is' (n1 * n0 :: stk')
     | _ => None
     end
-  | ICopy i :: aIs' =>
+  | ICopy i :: a_is' =>
     match nth i (map Some stk) None with
-    | Some n => aEval' aIs' (n :: stk)
+    | Some n => a_eval' a_is' (n :: stk)
     | _ => None
     end
-  | ISlide i :: aIs' =>
+  | ISlide i :: a_is' =>
     match stk with
-    | n :: stk' => aEval' aIs' (n :: skipn i stk')
+    | n :: stk' => a_eval' a_is' (n :: skipn i stk')
     | _ => None
     end
+  | IFail :: _ => None
   end.
 
-Definition aEval (state : list aInstruction * list nat) : option nat :=
+Definition a_eval (state : list a_instruction * list nat) : option nat :=
   match state with
-  | (aIs, stk) => aEval' aIs stk
+  | (a_is, stk) => a_eval' a_is stk
   end
 .
 
-Fixpoint envOffset (o : nat) (e : list (string * nat)) : list (string * nat) :=
+Definition a_com_env := list (string * nat).
+
+Fixpoint env_offset (o : nat) (e : a_com_env) : a_com_env :=
   match e with
   | [] => []
-  | (id, n) :: e' => (id, n + o) :: envOffset o e'
+  | (id, n) :: e' => (id, n + o) :: env_offset o e'
   end
 .
 
-Fixpoint aCompileExpr (e : aExpr) (env : list (string * nat)) : list aInstruction :=
+Fixpoint a_compile' (e : a_expr) (env : a_com_env) : list a_instruction :=
   match e with
   | Num n => [INum n]
-  | Plus e1 e2 => aCompileExpr e1 env ++ aCompileExpr e2 (envOffset 1 env) ++ [IPlus]
-  | Mult e1 e2 => aCompileExpr e1 env ++ aCompileExpr e2 (envOffset 1 env) ++ [IMult]
+  | Plus e1 e2 => a_compile' e1 env ++ a_compile' e2 (env_offset 1 env) ++ [IPlus]
+  | Mult e1 e2 => a_compile' e1 env ++ a_compile' e2 (env_offset 1 env) ++ [IMult]
   | ALet bs eBody =>
-    let bLength := List.length bs in
-    let bFolder :=
+    let b_length := length bs in
+    let b_folder :=
         fun acc b =>
           match acc with
-          | (n, f) => (n - 1, aCompileExpr (snd b) (envOffset n env) ++ f)
+          | (n, f) => (n - 1, a_compile' (snd b) (env_offset n env) ++ f)
           end in
-    let bIs := snd (fold_left bFolder bs (bLength - 1, [])) in
-    let bEnv := combine (map fst bs) (rev (seq 0 bLength)) ++ (envOffset bLength env) in
-    bIs ++ aCompileExpr eBody bEnv ++ [ISlide bLength]
+    let b_is := snd (fold_left b_folder bs (b_length - 1, [])) in
+    let b_env := combine (map fst bs) (rev (seq 0 b_length)) ++ (env_offset b_length env) in
+    b_is ++ a_compile' eBody b_env ++ [ISlide b_length]
   | AVar id =>
-    match find (compose (flip beq_string id) fst) env with
+    match find (compose (eqb id) fst) env with
       | Some (_, i) => [ICopy i]
-      | _ => []
+      | _ => [IFail]
     end
   end
 .
 
-Definition aCompile (e : aExpr) : list aInstruction := aCompileExpr e [].
+Definition a_compile (e : a_expr) : list a_instruction := a_compile' e [].
 
-Lemma interpret_compile_equiv_general : forall (aIs : list aInstruction) (stk : list nat) (e : aExpr),
-    match aInterpret e with
-    | Some n => aEval (aIs, n :: stk) = aEval (aCompile e ++ aIs, stk)
+Definition int_env_to_com_env (int_env : a_int_env) : a_com_env := combine (map fst int_env) (rev (seq 0 (length int_env))).
+
+Definition int_env_to_stack (int_env : a_int_env) : a_stack := rev (map snd int_env).
+
+Definition com_env_stack_to_int_env (com_env : a_com_env) (stack : a_stack) : option a_int_env :=
+  let max_ind := fold_left max (map (compose (plus 1) snd) com_env) 0 in
+  if max_ind <=? length stack
+  then Some (map (fun b => (fst b, nth (snd b) stack 0)) com_env)
+  else None
+.
+
+(*
+Lemma interpret_compile_equiv_general : forall (a_is : list a_instruction) (stk : a_stack) (e : a_expr),
+    match a_interpret e with
+    | Some n => a_eval (a_is, n :: stk) = a_eval (a_compile e ++ a_is, stk)
     | _ => True
     end
 .
 Proof.
-  intros; generalize dependent stk; generalize dependent aIs.
+  intros; generalize dependent stk; generalize dependent a_is.
   induction e; intros; simpl; try auto;
-    try ( unfold aInterpret; unfold aInterpret in IHe1; unfold aInterpret in IHe2; simpl;
-          destruct (aInterpret' e1 []); destruct (aInterpret' e2 []); auto;
-          unfold aCompile; simpl; unfold aEval in IHe1; unfold aEval in IHe2;
+    try ( unfold a_interpret; unfold a_interpret in IHe1; unfold a_interpret in IHe2; simpl;
+          destruct (a_interpret' e1 []); destruct (a_interpret' e2 []); auto;
+          unfold a_compile; simpl; unfold a_eval in IHe1; unfold a_eval in IHe2;
           repeat rewrite app_assoc_reverse; rewrite <- IHe1; rewrite <- IHe2; now auto
         ).
-  unfold aInterpret; unfold aInterpret in IHe; simpl.
-  (* Need some lemma for `aInterpret e p` case *)
+  unfold a_interpret; unfold a_interpret in IHe; simpl.
+  (* Need some lemma for `a_interpret e p` case *)
 Abort.
-(* Qed. *)
+*)
 
 (*
-Theorem interpret_compile_equiv : forall (e : aExpr),
-    Some (aInterpret e) = aEval (aCompile e, [])
+Theorem interpret_compile_equiv : forall (e : a_expr),
+    Some (a_interpret e) = a_eval (a_compile e, [])
 .
 Proof.
   intros.
-  replace (aCompile e) with (aCompile e ++ []);
+  replace (a_compile e) with (a_compile e ++ []);
     auto with datatypes.
   rewrite <- interpret_compile_equiv_general.
   now auto.
 Qed.
- *)
+*)
